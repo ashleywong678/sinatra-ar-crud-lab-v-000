eed7fe1384baf6adf4d5415d208cfc10d9ab81c9
Ashley Wong <ashwong04@gmail.com>
2019-03-04 14:57:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do
     @article = Article.find(params[:id])
     @article.update(params[:article])
-    @article.save
+    # @article.save
     redirect to "/articles/#{article.id}"
   end
   
