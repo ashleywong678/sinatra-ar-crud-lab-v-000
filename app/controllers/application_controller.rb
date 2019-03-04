87efef69aa6136350ef4934787cec64bd26bb7b8
Ashley Wong <ashwong04@gmail.com>
2019-03-04 14:54:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,8 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do
     binding.pry
     @article = Article.find(params[:id])
-    @article.update(params)
+    @article.update(params[:article])
+    
   end
   
 end
