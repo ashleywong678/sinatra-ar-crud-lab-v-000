de9e36948c7d98125133d511152e152115c90cbc
Ashley Wong <ashwong04@gmail.com>
2019-03-04 15:04:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
   end
   
   delete '/articles/:id' do
-    @article = Article.find(params[:id])
+    Article.find(params[:id])
     
     erb :index
   end
